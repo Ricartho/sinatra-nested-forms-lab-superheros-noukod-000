fd3b003d32a77bfcdae5f0518298e2370e7a1733
Ricartho Franck <ricartho.franck@esih.edu>
2019-06-04 15:39:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,8 @@ class App < Sinatra::Base
       erb :super_hero
     end 
     
-    # post '/teams' do 
-    #   @team = Team.new(params[:team])
-      
-    #   erb :team
-
+    post '/teams' do 
+      #@team = Team.new(params[:team])
+      erb :team
+    end 
 end
